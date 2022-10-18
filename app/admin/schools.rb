161aad8d6bda4204887a01c8d59f4bab3e6a8f62
Bit-amarnath <amarnath.choudhary@bitcanny.com>
2022-10-18 18:10:02 +0530

    Done

---
+++ app/admin/schools.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register School do
+
+  permit_params :name, :student_number
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_121911) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_123048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_121911) do
     t.string "student_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "school_id", null: false
+    t.index ["school_id"], name: "index_students_on_school_id"
   end
 
+  add_foreign_key "students", "schools"
 end
