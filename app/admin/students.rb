df701b906ef41b1f60de8083daaeaead7ff33d05
Bit-amarnath <amarnath.choudhary@bitcanny.com>
2022-11-22 17:24:36 +0530

    customization of index page

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -13,4 +13,26 @@ ActiveAdmin.register Student do
   #   redirect_to import_csv_admin_students_path notice: "CSV imported successfully!"
   # end
 
+  index do
+    selectable_column
+    column :name
+  end
+  
+  index as: :grid do |student|
+    link_to student.name, admin_student_path(student)
+  end
+  
+  index as: :block do |student|
+    div for: student do
+      resource_selection_cell student
+      h2  auto_link     student.name
+      h4 simple_format student.student_number
+    end
+  end
+
+  index as: :blog do
+    :name
+  end
+  
+  
 end
